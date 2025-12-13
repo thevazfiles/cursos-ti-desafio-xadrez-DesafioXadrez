09e82c01001b2fa4e9189186d4c3acfd450660d3
JonhyVaz <86691888+thevazfiles@users.noreply.github.com>
2025-12-13 17:41:21 +0000

    Implementação de movimento CAVALO e retirada de informação de fim de movimentos peças.

--- xadrez.c
+++ xadrez.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 int main() {
 
     //Variáveis de controle para as estruturas de repetição.
@@ -8,18 +10,18 @@ int main() {
     char direita[] = "Direita";
     char esquerda[] = "Esquerda";
     char cima[] = "Cima";
+    char baixo[] = "Baixo";
+
+    // String de quantidade de casas do cavalo.
+    int casasVertical = 2; // Movimento Vertical: Dias casas para baixo.
+    int casasHorizontal = 1; // Movimento Horizontal: Uma casa para a esquerda.
 
     // Movimentação da TORRE: 5 casas para a direita.
     // Estrutura utilizada: FOR
-    printf("\n1 - Movimentação da Torre: \n");
+    printf("\n1 - Movimentação da TORRE: \n");
 
     for (int movimentoTorre = 0; movimentoTorre < 5; movimentoTorre++) {
-        printf("Move  uma casa: %s!\n", direita);
-
-        // Mensagem exibida somente ao final do último movimento.
-        if (movimentoTorre == 4) {
-            printf("A torre finaliza seu movimento!\n");
-        }
+        printf("%s\n", direita);
     }
 
     // Movimentação do BISPO: 5 casas na diagonal para a direita.
@@ -28,11 +30,9 @@ int main() {
     printf("\n2 - Movimentação do BISPO: \n");
 
     while (movimentoBispo < 5) {
-        printf("Move uma casa: %s, %s!\n", cima, direita);
+        printf("%s, %s\n", cima, direita);
         movimentoBispo++; // incrementa o contador do número de movimentos.
     }
-
-    printf("O bispo terminou sua movimentação! \n");
 
     // Movimentação da RAINHA: 8 casas para a esquerda.
     // Estrutura utilizada: DO-WHILE
@@ -40,11 +40,30 @@ int main() {
     printf("\n3 - Movimentação da RAINHA: \n");
 
     do{
-        printf("Move uma casa: %s!\n", esquerda);
+        printf("%s\n", esquerda);
         movimentoRainha++; // incrementa o contador do número de movimentos.
-    } while (movimentoRainha < 8);
+    } while (movimentoRainha < 8); 
 
-    printf("A rainha terminou sua movimentação\n");
+    // Movimentação do CAVALO: 2 casas para baixo. - Estrutura utilizada: FOR
+    // Movimentação do CAVALO: 1 casa para a esquerda. - Estrutura utilizada: WHILE
+
+    printf("\n4 - Movimentação do CAVALO");
+
+    // Loop Externo (FOR): Simula as 2 casas para BAIXO (Movimento Vertical)
+    // Utilizado o FOR, pois o número de repetições (casasVertical = 2) é fixo.
+    for (int i = 0; i < casasVertical; i++) {
+        printf("%s\n", baixo);
+
+        // Após concluir o movimento vertical, executa o movimento horizontal.
+        if (i == casasVertical - 1) {
+            int j = 0;
+            while (j < casasHorizontal) {
+                printf("%s\n", esquerda);
+                j++;
+               
+            }
+        }
+    }
 
     return 0;
 }
